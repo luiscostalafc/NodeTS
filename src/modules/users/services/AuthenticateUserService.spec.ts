c4d0aa91b4c7ea6c58508ab4b10aa9132d53cb99
luiscostalafc <luiscostalafc@gmail.com>
2020-10-20 10:54:45 -0300

    Recuperaçao de senha

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -70,7 +70,7 @@ describe('AuthenticateUser', () => {
       password: '123456',
     });
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: 'wrong-password',
