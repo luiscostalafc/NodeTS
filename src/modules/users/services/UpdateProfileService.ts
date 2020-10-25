dcce7ce49ebb1b463d1abd8826325d7ee1de138d
luiscostalafc <luiscostalafc@gmail.com>
2020-10-25 19:24:01 -0300

    Refatoração dos testes

---
+++ src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,31 @@
+import { injectable, inject } from 'tsyringe';
+
+//import AppError from '@shared/errors/AppError';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider'
+import IUsersRepository from '../repositories/IUsersRepository';
+
+//import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+  old_password?: string;
+  password?: string;
+}
+
+@injectable()
+class UpdateProfile {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ user_id, name, email }: IRequest): Promise<void> {
+  }
+}
+
+export default UpdateProfile;
