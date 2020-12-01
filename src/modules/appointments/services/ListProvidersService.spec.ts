bebe3a3b46ae581e532fbbbda0b36cc13c7f190a
luiscostalafc <luiscostalafc@gmail.com>
2020-11-30 21:19:33 -0300

    Cache lista de providers

--- src/modules/appointments/services/ListProvidersService.spec.ts
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -1,5 +1,3 @@
-//import AppError from '@shared/errors/AppError';
-
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import ListProvidersService from './ListProvidersService';
 
@@ -36,7 +34,5 @@ describe('ListProvider', () => {
     });
 
     expect(providers).toEqual([user1, user2]);
-
   });
-
 });
