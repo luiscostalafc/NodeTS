d4d7a2d64cc67fabdc42970d5bd4020765e95192
luiscostalafc <luiscostalafc@gmail.com>
2020-12-06 09:08:39 -0300

    Agendamentos no mesmo horário

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -2,10 +2,11 @@ import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
 import IFindALlInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
+import { String } from 'aws-sdk/clients/apigateway';
 
 export default interface IAppoinmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
-  findByDate(date: Date): Promise<Appointment | undefined>;
+  findByDate(date: Date, provider_id: String): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(
     data: IFindAllInMonthFromProviderDTO,
   ): Promise<Appointment[]>;
